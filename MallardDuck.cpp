7fe3609747e8370c8ac059f5d6b45a60f4752212
GillzaSVD <cf_lego@mail.ru>
2021-09-06 12:32:38 +0700

    добавить класс MallardDuck

---
+++ MallardDuck.cpp
@@ -0,0 +1,9 @@
+#include "MallardDuck.h"
+
+MallardDuck::MallardDuck() {
+
+}
+
+void MallardDuck::display() {
+	cout << "I'm MallardDuck" << endl;
+}
---
+++ MallardDuck.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Duck.h"
+
+class MallardDuck
+{
+public:
+	MallardDuck();
+	void display();
+};
+
